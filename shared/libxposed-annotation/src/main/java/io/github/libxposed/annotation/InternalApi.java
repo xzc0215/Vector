989a7e75c0fa95f573fa49192b38e0bc5f5a60b0
xzc0215 <94352968+xzc0215@users.noreply.github.com>
2026-07-04 02:57:16 +0000

    Create InternalApi.java

---
+++ shared/libxposed-annotation/src/main/java/io/github/libxposed/annotation/InternalApi.java
@@ -0,0 +1,18 @@
+package io.github.libxposed.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({
+    ElementType.TYPE,
+    ElementType.FIELD,
+    ElementType.METHOD,
+    ElementType.CONSTRUCTOR,
+})
+public @interface InternalApi {
+}
